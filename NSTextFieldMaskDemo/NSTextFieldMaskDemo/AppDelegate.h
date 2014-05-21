9ecd1357b4f5deb0fa107bc55384401b359ecaac
Andrii Tishchenko <andrii.tishchenko+github@gmail.com>
2014-05-21 21:49:10 +0300

    demo project

---
+++ NSTextFieldMaskDemo/NSTextFieldMaskDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSTextFieldMaskDemo
+//
+//  Created by Andrii Tishchenko on 05.02.14.
+//  Copyright (c) 2014 Andrii Tishchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NSTextFieldMaskDemo/NSTextFieldMaskDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSTextFieldMaskDemo
+//
+//  Created by Andrii Tishchenko on 05.02.14.
+//  Copyright (c) 2014 Andrii Tishchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "NSTextFieldMask.h"
+
+@interface ViewController : UIViewController<NSTextFieldMaskDelegate>
+@property (weak, nonatomic) IBOutlet NSTextFieldMask *test;
+
+@end
